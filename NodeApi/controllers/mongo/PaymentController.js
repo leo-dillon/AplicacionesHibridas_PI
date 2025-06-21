9eaba0b1907a6c62eb948e320426ac0a771494b7
Ticiano Piccino <piccinoticiano@gmail.com>
2025-06-21 13:43:02 -0300

    arreglo de fecha de vencimiento

--- NodeApi/controllers/mongo/PaymentController.js
+++ NodeApi/controllers/mongo/PaymentController.js
@@ -75,31 +75,37 @@ static async getPaymentidPaid(req, res) {
         });
     }
 }
-    static async createPayment( req, res ){
-        const user_id = req.body.issuedTo
-
-        const exist = await PaymentModel.findOne({
-            issuedTo: user_id,
-            paymentType: 'create_school',
-            status: 'pending'
-        });
-    
-        if (exist) {
-            return res.status(400).json({
-                message: 'Ya generaste una solicitud de pago. Debe ser abonada primero.'
-            });
-        }
-
-        const payment = await PaymentModel.create({
-            issuedTo: user_id,
-            concept: 'Creación de escuela',
-            amount: 5000,
-            dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 días
-            paymentType: 'create_school'
-        });
-        payment.save()
-        res.status(200).json({ message: 'Solicitud de pago generada', payment });
+    static async createPayment(req, res) {
+  try {
+    const { issuedTo, schoolId, concept, amount, dueDate, paymentMethod, paymentType } = req.body;
+    const exist = await PaymentModel.findOne({
+      issuedTo,
+      paymentType: 'create_school',
+      status: 'pending'
+    });
+    if (exist) {
+      return res.status(400).json({
+        message: 'Ya generaste una solicitud de pago. Debe ser abonada primero.'
+      });
     }
+
+    const payment = new PaymentModel({
+      issuedTo,
+      schoolId: schoolId || null,
+      concept,
+      amount,
+      dueDate,
+      paymentMethod: paymentMethod || null,
+      paymentType
+    });
+
+    await payment.save();
+    res.status(201).json({ message: 'Solicitud de pago generada', payment });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error al crear el pago', error });
+  }
+}
 
 
     static async editPayment (req, res){
